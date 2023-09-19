887f0ff4ec641d42971ea621b801556c464fde6d
root <root@control.tsengle-167840.hyflowtm-pg0.utah.cloudlab.us>
2023-09-19 15:05:39 -0600

    final WAN plot

---
+++ WAN-tpt.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+  
+# creating the dataset
+#data = {'PQR (0.125)': 21632.61, 'Pineapple (0.125)': 30760.99, 'EPaxos (0.125)': 14142.10, 'PQR (0.25)': 20909.21, 'Pineapple (0.25)':  25733.42, 'EPaxos (0.25)': 14721.87, 'PQR (0.5)': 20031.99, 'Pineapple (0.5)': 22391.78, 'EPaxos (0.5)': 13455.03, 'PQR (1.0)': 18397.87, 'Pineapple (1.0)': 18304.74, 'EPaxos (1.0)': 14657.05}
+data = {'PQR': 12243.24, 'Pineapple': 18578.16, 'Gryff': 14830.22}
+protocol = list(data.keys())
+tpt = list(data.values())
+  
+fig = plt.figure(figsize = (15, 2))
+ 
+# creating the bar plot
+bars = plt.bar(protocol, tpt, 
+        width = 0.8)
+
+for x in range(1):
+    bars[3*x].set_color('blue')
+    bars[3*x+1].set_color('orange')
+    bars[3*x+2].set_color('green')
+
+plt.xticks(rotation=25)
+
+ 
+plt.xlabel("Systems")
+plt.ylabel("Throughput (Ops/sec)")
+#plt.title("Students enrolled in different courses")
+#plt.show()
+plt.savefig("WAN-test.png", bbox_inches="tight")
+
+
+#bars = plt.bar(protocol, tpt, 
+#        width = 1.5)
+#plt.savefig("test3-1.5.png", bbox_inches="tight")
