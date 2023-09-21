ee9e5b9e37174c6035e6acf503d8a072e83a581c
root <root@control.tsengle-168159.hyflowtm-pg0.wisc.cloudlab.us>
2023-09-20 23:42:13 -0500

    update line plot

--- line.py
+++ line.py
@@ -7,12 +7,20 @@ x = [6446, 11331, 13114, 13872, 14164, 14304, 14714, 15539, 15921]
 y1 = [0.35, 0.49, 0.83, 1.23, 2.59, 1.96, 2.58, 3.89, 5.15]
 x1 = [8033, 17373, 20270, 20657, 21435, 21813, 22263, 22377, 22680]
 
-fig = plt.figure(figsize = (10, 3))
+#fig = plt.figure(figsize = (10, 3))
 
-plt.plot(x, y, color='green', linestyle="dashdot", label="Gryff")
-plt.plot(x1,y1, color='orange', linestyle="solid", label="Pineapple")
+fig, ax = plt.subplots()
 
-plt.xlabel("Latency (ms)")
-plt.ylabel("Throughput (Ops/sec)")
+fig.set_figheight(10)
+fig.set_figwidth(3)
 
-plt.savefig("latency-tput.png", bbox_inches="tight")
+
+ax.plot(x, y, color='green', linestyle="dashdot", label="Gryff")
+ax.plot(x1,y1, color='orange', linestyle="solid", label="Pineapple")
+
+ax.set_xlabel("Latency (ms)")
+ax.set_ylabel("Throughput (Ops/sec)")
+
+ax.legend()
+
+fig.savefig("latency-tput.png", bbox_inches="tight")
